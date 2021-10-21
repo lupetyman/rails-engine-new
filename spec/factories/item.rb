e4c65451b1b1b7059fb3fa738e86504db1b40335
lupetyman <lupetyman@gmail.com>
2021-10-21 13:25:00 -0500

    Set up factories and associations

---
+++ spec/factories/item.rb
@@ -0,0 +1,8 @@
+FactoryBot.define do
+  factory :item do
+    name { ker::Food.dish }
+    description { Faker::Food.description }
+    unit_price { Faker::Number.decimal(l_digits: 3, r_digits: 2) }
+    merchant
+  end
+end
