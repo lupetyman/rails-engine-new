88cef59aaa74b9ce5df0b4334ae75973f1eb7a64
lupetyman <lupetyman@gmail.com>
2021-10-23 15:46:27 -0500

    Complete all merchants request

--- app/controllers/api/v1/merchants_controller.rb
+++ app/controllers/api/v1/merchants_controller.rb
@@ -1,6 +1,17 @@
 class Api::V1::MerchantsController < ApplicationController
   def index
-    @merchants = Merchant.paginate(page: 1, per_page: 20)
-    render json: MerchantSerializer.new(@merchants)
+    if params[:page] && params[:per_page]
+      merchants = Merchant.paginate(page: params[:page], per_page: params[:per_page])
+      render json: MerchantSerializer.new(merchants)
+    elsif params[:per_page]
+      merchants = Merchant.paginate(page: 1, per_page: params[:per_page])
+      render json: MerchantSerializer.new(merchants)
+    elsif params[:page]
+      merchants = Merchant.paginate(page: params[:page], per_page: 20)
+      render json: MerchantSerializer.new(merchants)
+    else
+      merchants = Merchant.paginate(page: 1, per_page: 20)
+      render json: MerchantSerializer.new(merchants)
+    end
   end
 end
--- spec/requests/api/v1/merchants/index_spec.rb
+++ spec/requests/api/v1/merchants/index_spec.rb
@@ -30,7 +30,7 @@ RSpec.describe 'Merchants API Requests' do
     end
 
     context 'when no data is available' do
-      it 'returns an empty array' do
+      it 'sends an empty array' do
         get '/api/v1/merchants'
 
         merchants = JSON.parse(response.body, symbolize_names: true)
@@ -39,9 +39,100 @@ RSpec.describe 'Merchants API Requests' do
         expect(merchants[:data]).to eq([])
       end
     end
-    #
-    # context 'when params are present' do
-    #
-    # end
+
+    context 'when params are present' do
+      context 'when both params are specified' do
+        it 'sends the appropriate list of merchants based on params' do
+          create_list(:merchant, 30)
+
+          all_merchants = Merchant.all
+
+          get '/api/v1/merchants', params: { per_page: 10, page: 2 }
+
+          merchants = JSON.parse(response.body, symbolize_names: true)
+
+          expect(response).to be_successful
+          expect(merchants[:data].count).to eq(10)
+          expect(merchants[:data].first[:id]).to eq(all_merchants[10].id.to_s)
+          expect(merchants[:data].last[:id]).to eq(all_merchants[19].id.to_s)
+
+          merchants[:data].each do |merchant|
+            expect(merchant).to have_key(:id)
+            expect(merchant[:id]).to be_an(String)
+
+            expect(merchant).to have_key(:type)
+            expect(merchant[:type]).to eq('merchant')
+
+            expect(merchant).to have_key(:attributes)
+            expect(merchant[:attributes]).to be_a(Hash)
+
+            expect(merchant[:attributes]).to have_key(:name)
+            expect(merchant[:attributes][:name]).to be_an(String)
+          end
+        end
+      end
+
+      context 'only when per_page is specified' do
+        it 'sends the appropriate list of merchants, page defaults to 1' do
+          create_list(:merchant, 30)
+
+          all_merchants = Merchant.all
+
+          get '/api/v1/merchants', params: { per_page: 10 }
+
+          merchants = JSON.parse(response.body, symbolize_names: true)
+
+          expect(response).to be_successful
+          expect(merchants[:data].count).to eq(10)
+          expect(merchants[:data].first[:id]).to eq(all_merchants[0].id.to_s)
+          expect(merchants[:data].last[:id]).to eq(all_merchants[9].id.to_s)
+
+          merchants[:data].each do |merchant|
+            expect(merchant).to have_key(:id)
+            expect(merchant[:id]).to be_an(String)
+
+            expect(merchant).to have_key(:type)
+            expect(merchant[:type]).to eq('merchant')
+
+            expect(merchant).to have_key(:attributes)
+            expect(merchant[:attributes]).to be_a(Hash)
+
+            expect(merchant[:attributes]).to have_key(:name)
+            expect(merchant[:attributes][:name]).to be_an(String)
+          end
+        end
+      end
+
+      context 'only when page is specified' do
+        it 'sends the appropriate list of merchants, per_page defaults to 20' do
+          create_list(:merchant, 30)
+
+          all_merchants = Merchant.all
+
+          get '/api/v1/merchants', params: { page: 2 }
+
+          merchants = JSON.parse(response.body, symbolize_names: true)
+
+          expect(response).to be_successful
+          expect(merchants[:data].count).to eq(10)
+          expect(merchants[:data].first[:id]).to eq(all_merchants[20].id.to_s)
+          expect(merchants[:data].last[:id]).to eq(all_merchants[29].id.to_s)
+
+          merchants[:data].each do |merchant|
+            expect(merchant).to have_key(:id)
+            expect(merchant[:id]).to be_an(String)
+
+            expect(merchant).to have_key(:type)
+            expect(merchant[:type]).to eq('merchant')
+
+            expect(merchant).to have_key(:attributes)
+            expect(merchant[:attributes]).to be_a(Hash)
+
+            expect(merchant[:attributes]).to have_key(:name)
+            expect(merchant[:attributes][:name]).to be_an(String)
+          end
+        end
+      end
+    end
   end
 end
