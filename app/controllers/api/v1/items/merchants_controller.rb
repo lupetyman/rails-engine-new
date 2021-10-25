fb0e9eac715ff88fd7c319fa80c6f78cdf87e226
lupetyman <lupetyman@gmail.com>
2021-10-25 15:08:54 -0500

    Complete 'GET /items/:id/merchant'

---
+++ app/controllers/api/v1/items/merchants_controller.rb
@@ -0,0 +1,7 @@
+class Api::V1::Items::MerchantsController < ApplicationController
+  def index
+    item = Item.find(params[:id])
+    merchant = Merchant.find(item.merchant_id)
+    json_response(MerchantSerializer.new(merchant))
+  end
+end
