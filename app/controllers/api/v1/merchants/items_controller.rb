c76036b6c09bd83c4a403efa582ed128a8f6ea7a
lupetyman <lupetyman@gmail.com>
2021-10-28 18:20:08 -0500

    Rubocop 100%

--- app/controllers/api/v1/merchants/items_controller.rb
+++ app/controllers/api/v1/merchants/items_controller.rb
@@ -1,14 +1,12 @@
 class Api::V1::Merchants::ItemsController < ApplicationController
   def index
     if merchant_id_a_string
-      return json_response({ error: "Couldn't find Merchant" }, :not_found)
+      json_response({ error: "Couldn't find Merchant" }, :not_found)
     else
       merchant = Merchant.find(params[:id])
       items = merchant.items
       json_response(ItemSerializer.new(items))
     end
-
-
   end
 
   private
