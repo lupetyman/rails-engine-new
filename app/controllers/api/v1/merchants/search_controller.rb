0fdde08d9da1fc5080c72160b6e6cfa01e4976db
lupetyman <lupetyman@gmail.com>
2021-10-29 13:08:59 -0500

    Clean up

--- app/controllers/api/v1/merchants/search_controller.rb
+++ app/controllers/api/v1/merchants/search_controller.rb
@@ -2,12 +2,12 @@ class Api::V1::Merchants::SearchController < ApplicationController
   def find
     return json_response({ error: 'Bad request' }, :bad_request) unless params[:name].present?
 
-    @merchant = Merchant.find_by_name(params[:name])
+    merchant = Merchant.find_by_name(params[:name])
 
-    if @merchant.nil?
+    if merchant.nil?
       json_response({ data: {} })
     else
-      json_response(MerchantSerializer.new(@merchant))
+      json_response(MerchantSerializer.new(merchant))
     end
   end
 end
